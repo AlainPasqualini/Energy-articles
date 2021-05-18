18a60efbb1648c2e2492d391d1db97c400def211
Alain Pasqualini <83455950+AlainPasqualini@users.noreply.github.com>
2021-05-18 07:57:52 +0200

    Update test_interactive.py

--- test_interactive.py
+++ test_interactive.py
@@ -2,11 +2,9 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import pydeck as pdk 
-import altair as alt 
-from datetime import datetime
 
 
-from __future__ import print_function
+#from __future__ import print_function
 from ipywidgets import interact, interactive, fixed, interact_manual
 import pandas as pd
 df=pd.DataFrame([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
